b31d7df44f0918367baad36b5a565a2bf9c92225
afrivera <andresrfelip@hotmail.com>
2022-06-28 18:04:44 -0500

    end session 7

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
+declare function customInitFunction(): Function;
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -15,6 +16,7 @@ export class PagesComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    customInitFunction();
   }
 
 }
