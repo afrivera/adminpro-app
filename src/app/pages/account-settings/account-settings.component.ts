bd039598d44e52974d9fdd4981661c7f96e02b1e
afrivera <andresrfelip@hotmail.com>
2022-06-28 10:46:17 -0500

    change design theme

---
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-settings',
+  templateUrl: './account-settings.component.html',
+  styles: [
+  ]
+})
+export class AccountSettingsComponent implements OnInit {
+
+  linkTheme = document.querySelector('#theme');
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  changeTheme( theme: string){
+
+    const url = `./assets/css/colors/${ theme }.css`;
+
+    this.linkTheme?.setAttribute('href', url);
+    localStorage.setItem('theme', url );
+
+  }
+
+}
