433a43b51ed4694f1c55c51cb43b8c5adee93004
afrivera <andresrfelip@hotmail.com>
2022-07-11 22:17:14 -0500

    config update image of user

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user.model';
+import { FileUploadService } from '../../services/file-upload.service';
 
 @Component({
   selector: 'app-profile',
@@ -13,10 +14,13 @@ export class ProfileComponent implements OnInit {
 
   public profileForm!: FormGroup;
   public user: User;
+  public imagUpload!: File;
+  public imageTemp: any = null;
 
   constructor(
     private fb: FormBuilder,
-    private userService: UserService
+    private userService: UserService,
+    private fileUploaService: FileUploadService
   ) { 
     this.user = userService.user;
   }
@@ -37,6 +41,24 @@ export class ProfileComponent implements OnInit {
         this.user.name = name;
         this.user.email = email;
       })
+  }
+
+  changeImage( file: File ): null | void{
+    this.imagUpload = file; 
+    if( !file ){
+      return this.imageTemp = null;
+    }   
+
+    const reader = new FileReader();
+    reader.readAsDataURL( file );
+    reader.onloadend = ()=> {
+      this.imageTemp = reader.result
+    }
+  }
+
+  uploadImage(){
+    this.fileUploaService.updatePhoto(this.imagUpload, 'users', this.user.uid!)
+      .then(image => this.user.image = image)
   }
 
 }
---
+++ src/app/services/file-upload.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+
+const base_url = environment.baseUrl;
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+
+  constructor() { }
+
+  async updatePhoto(
+    file: File,
+    type: 'users' | 'doctors' | 'hospitals',
+    id: string
+  ){
+    try {
+      const url = `${base_url}/uploads/${ type }/${ id }`;
+
+      const formData = new FormData();
+      formData.append('image', file);
+
+      const resp = await fetch( url, {
+        method: 'PUT',
+        headers:{
+          'x-token': localStorage.getItem('x-token') || ''
+        },
+        body: formData
+      });
+
+      const data = await resp.json();
+      if( data.status ){
+        return data.body.image
+      } else {
+        console.log(data.message)
+        return false;
+      }
+
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+  }
+}
