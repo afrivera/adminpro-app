c34580f77e9d94e10f932bf1fa505d50294305d8
afrivera <andresrfelip@hotmail.com>
2022-07-23 17:24:17 -0500

    config get service doctors, component and html with image service

--- src/app/pages/manteinances/doctors/doctors.component.ts
+++ src/app/pages/manteinances/doctors/doctors.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { DoctorService } from '../../../services/doctor.service';
+import { ModalImageService } from '../../../services/modal-image.service';
+
+import { Doctor } from '../../../models/doctor.model';
 
 @Component({
   selector: 'app-doctors',
@@ -8,9 +14,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorsComponent implements OnInit {
 
-  constructor() { }
+  public loading: boolean = true;
+  public doctors: Doctor[] = [];
+  private imgSubs!: Subscription;
+  public doctorsTemp: Doctor[] = [];
+
+  constructor(
+    private doctorService: DoctorService,
+    private modalImageService: ModalImageService
+  ) { }
 
   ngOnInit(): void {
+    this.loadDoctors()
+  }
+
+  loadDoctors(){
+    this.loading = true;
+    this.doctorService.loadDoctors()
+      .subscribe( doctors => {
+        console.log(doctors)
+        this.loading = false;
+        this.doctors = doctors;
+        this.doctorsTemp = doctors;
+      })
+  }
+
+  openModal(doctor: Doctor){
+    this.modalImageService.openModal('doctors', doctor._id!, doctor.image)
   }
 
 }
--- src/app/services/doctor.service.ts
+++ src/app/services/doctor.service.ts
@@ -1,11 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.prod';
+import { map, Observable } from 'rxjs';
+import { Doctor } from '../models/doctor.model';
 
+
+const base_url = environment.baseUrl;
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorService {
 
   constructor(
-    
+    private http: HttpClient
   ) { }
+
+  get token( ){
+    return localStorage.getItem('x-token') || '';
+  }
+  
+  get headers(){
+    return {
+      headers: {
+        'x-token': this.token
+      }
+    }
+  }
+
+  loadDoctors(): Observable<Doctor[]>{
+    const url = `${ base_url }/doctors`;
+    return this.http.get( url, this.headers )
+      .pipe(
+        map((res: any) => res.body)
+      )
+  }
 }
