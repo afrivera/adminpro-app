9b4df220dd6fb796edf28e475984c7430025fb0b
afrivera <andresrfelip@hotmail.com>
2022-07-24 14:55:01 -0500

    config html of doctor component

--- src/app/pages/manteinances/doctors/doctor.component.ts
+++ src/app/pages/manteinances/doctors/doctor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HospitalService } from '../../../services/hospital.service';
+import { Hospital } from '../../../models/hospital.model';
 
 @Component({
   selector: 'app-doctor',
@@ -8,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorComponent implements OnInit {
 
-  constructor() { }
+  public doctorForm!: FormGroup;
+  public hospitals: Hospital[] = [];
+  public hospitalSelect!: Hospital;
+
+  constructor(
+   private fb: FormBuilder,
+   private hospitalService: HospitalService
+  ) { }
 
   ngOnInit(): void {
+    this.loadHospitals()
+    this.doctorForm = this.fb.group({
+      name: ['Cristian', Validators.required],
+      hospital: ['', Validators.required],
+    })
+
+    this.doctorForm.get('hospital')?.valueChanges
+      .subscribe(hospitalId => {
+        this.hospitalSelect = this.hospitals.find( h => h._id === hospitalId)!
+      })
+  }
+
+  loadHospitals(){
+    this.hospitalService.loadHospitals()
+      .subscribe( res => this.hospitals = res)
+  }
+
+  saveDoctor(){
+    console.log(this.doctorForm.value)
   }
 
 }
